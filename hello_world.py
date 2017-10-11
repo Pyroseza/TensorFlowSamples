95bed67bacb07d404e01d7727365f818ac03dd98
Pyroseza <jarpri08@gmail.com>
2017-10-11 22:38:16 +0100

    whoops comments were wrong

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-//turn off warnings for when run on Windows
+#turn off warnings for when run on Windows
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
-//script really starts here
+#script really starts here
 import tensorflow as tf
 
 hello = tf.constant('Hello, TensorFlow!')
